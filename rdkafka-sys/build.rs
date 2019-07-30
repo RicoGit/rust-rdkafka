e2c2e3af3d295db1c96588214dadb4018a36adb6
C.Solovev <constantine.solovev@gmail.com>
2019-07-30 16:40:40 +0400

    Support for librdkafka 1.1.0

--- rdkafka-sys/build.rs
+++ rdkafka-sys/build.rs
@@ -55,7 +55,6 @@ fn main() {
     } else {
         if !Path::new("librdkafka/LICENSE").exists() {
             println_stderr!("Setting up submodules");
-            run_command_or_fail("../", "git", &["submodule", "update", "--init"]);
             run_command_or_fail("../", "git", &["submodule", "update", "--remote"]);
         }
         println_stderr!("Building and linking librdkafka statically");
